a94a4457bb7dc93888125b91f56e876f2a4ee781
t0fune <106223309+t0fune@users.noreply.github.com>
2025-07-22 12:15:41 +0700

    Create Exercise5_2_kornrapart_r.py

---
+++ Exercise5_2_kornrapart_r.py
@@ -0,0 +1,12 @@
+x = int(input("Number 1 ="))
+y = int(input("Number 2 ="))
+
+result1 = x+y
+result2 = x-y
+result3 = x*y
+result4 = x/y
+
+print("result :",result1)
+print("result :",result2)
+print("result :",result3)
+print("result :",result4)
