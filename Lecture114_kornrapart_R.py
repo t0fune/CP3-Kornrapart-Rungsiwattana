521da8e5c96e37412b06b433eacf009eedc2706c
t0fune <106223309+t0fune@users.noreply.github.com>
2025-08-13 20:06:34 +0700

    Update Lecture114_kornrapart_R.py

--- Lecture114_kornrapart_R.py
+++ Lecture114_kornrapart_R.py
@@ -3,27 +3,27 @@ from datetime import datetime
 from forex_python.converter import CurrencyRates, get_currency_name
 
 
-def currencyConvert():
+def currency_convert():
     print("Currency Converter From Now".center(50,"-"))
-    cConvert = CurrencyRates()
-    result = cConvert.get_rate(input('Which Currency would you like to convert : '), input('to : '))
+    c_convert = CurrencyRates()
+    result = c_convert.get_rate(input('Which Currency would you like to convert : '), input('to : '))
     print(result)
 
-def currencyValueAtThatTime ():
+def currency_value_at_that_time ():
     print("Currency Converter From Past".center(50,"-"))
-    cCurrencyValueAtThatTime = CurrencyRates()
-    dateCurrency = datetime(int(input('Year : ')), int(input('Month : ')), int(input('Date : ')), )
-    currencyValue = cCurrencyValueAtThatTime.get_rate(input('Which Currency would you like to convert : '), input('to : '),dateCurrency)
-    print(currencyValue)
+    c_currency_value_at_that_time = CurrencyRates()
+    date_currency = datetime(int(input('Year : ')), int(input('Month : ')), int(input('Date : ')), )
+    currency_value = c_currency_value_at_that_time.get_rate(input('Which Currency would you like to convert : '), input('to : '),date_currency)
+    print(currency_value)
 
-def currencyCalculator():
+def currency_calculator():
     print("How much Currency Increases or Decreases From Past".center(50,"-"))
-    cCalculator = CurrencyRates()
-    currencyVariable1 = input('Which Currency would you like to convert : ')
-    currencyVariable2 = input('to : ')
-    currencyValueNowAndPast = cCalculator.get_rate(currencyVariable1,currencyVariable2,datetime(int(input('Year : ')), int(input('Month : ')), int(input('Date : ')), ))
-    currencyValuePerYear = ((cCalculator.get_rate(currencyVariable1,currencyVariable2)-currencyValueNowAndPast)/currencyValueNowAndPast)*100
-    print(currencyValuePerYear.__round__(2),"%")
+    c_currency_calculator = CurrencyRates()
+    currency_variable_1 = input('Which Currency would you like to convert : ')
+    currency_variable_2 = input('to : ')
+    currency_value_now_and_past = c_currency_calculator.get_rate(currency_variable_1,currency_variable_2,datetime(int(input('Year : ')), int(input('Month : ')), int(input('Date : ')), ))
+    currency_value_per_year = ((c_currency_calculator.get_rate(currency_variable_1,currency_variable_2)-currency_value_now_and_past)/currency_value_now_and_past)*100
+    print(currency_value_per_year.__round__(2),"%")
 
 
 print("Menu Select".center(30, "-"))
@@ -34,6 +34,6 @@ print("3. How much Currency Increases or Decreases From Past")
 
 print("-".center(30, "-"))
 selectMenu = input("Select an option : ")
-if selectMenu == "1": currencyConvert()
-elif selectMenu == "2": currencyValueAtThatTime()
-elif selectMenu == "3": currencyCalculator()
+if selectMenu == "1": currency_convert()
+elif selectMenu == "2": currency_value_at_that_time ()
+elif selectMenu == "3": currency_calculator()
